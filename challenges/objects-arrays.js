7050a53b02ad0e1b50536eaaf8e5799654f15ace
Valerie Vazquez <vnvazquez68@gmail.com>
2020-03-21 14:48:43 -0600

    Finished Project

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -11,7 +11,8 @@ let tRex = {
   diet : "carnivorous",
   weight : 7000,
   length : 12,
-  period : "Late Cretaceous"
+  period : "Late Cretaceous",
+  roar: "RAWERSRARARWERSARARARRRR!"
 }
 
 let steg = {
@@ -52,7 +53,8 @@ console.log(tRex.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tRex.roar)
+
 
 
 // ==== Arrays ====
@@ -77,21 +79,12 @@ const graduates = [
 
 Once you have the new array created, log the result. */
  
-// function getFullNames(runners) {
-//  const fullName = [];
-//   runners.forEach((runners) => {fullName.push(`${runners.last_name}, ${runners.first_name}`)});
-//   return fullName;
-// }
-
-
 const universities = [];
 graduates.forEach (arrItem => {
   universities.push(`${arrItem.university}`);
 })
 console.log(universities);
 
-  // const universities = [];
-  // console.log (universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -99,6 +92,7 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
+
 const contactInfo = [];
 graduates.forEach(( array => {contactInfo.push(`${array.first_name}  ${array.email}`)}));
 console.log(contactInfo);
@@ -106,8 +100,13 @@ console.log(contactInfo);
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 
 
-const unisWithUni = [];
-console.log(unisWithUni);
+const uni = [];
+for (i = 0; i < graduates.length; i++) {
+  if (graduates[i].university.includes("Uni")) {
+    uni.push(graduates[i].university)
+  }
+}
+console.log(uni);
 
 
 // ==== ADVANCED Array Methods ====
